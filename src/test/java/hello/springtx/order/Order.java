035ca071b8ff8e754b69f02050056394be43817c
Junsik <wnstlr4045@naver.com>
2023-01-25 01:41:47 +0900

    Initial commit

---
+++ src/test/java/hello/springtx/order/Order.java
@@ -0,0 +1,27 @@
+package hello.springtx.order;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.quartz.QuartzTransactionManager;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username; //정상, 예외, 잔고부족
+    private String payStatus; //대기, 완료
+
+
+}
